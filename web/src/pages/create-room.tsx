50d929181547c08b7e843721aa7eae116e11f79b
Bruno Becoski <brunobecoski@gmail.com>
2025-07-10 16:08:18 -0300

    Adiciona o React Query

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,14 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
 
+type GetRoomsAPIResponse = Array<{
+	id: string;
+	name: string;
+}>;
+
 export function CreateRoom() {
+	const { data, isLoading } = useQuery({
+		queryKey: ["get-rooms"],
+		queryFn: async () => {
+			const response = await fetch("http://localhost:3333/rooms");
+			const result: GetRoomsAPIResponse = await response.json();
+
+			return result;
+		},
+	});
+
 	return (
-		<div>
-			<h1>Criar Sala</h1>
-			<Link to="/room">
-				<Button variant="link">Acessar Sala</Button>
-			</Link>
+		<div className="flex flex-col items-center h-screen w-screen gap-4">
+			<h1 className="text-4xl my-4">Criar Sala</h1>
+
+			{isLoading && <p className="animate-bounce">Carregando...</p>}
+
+			<div className="flex justify-center flex-wrap gap-2">
+				{data?.map((room) => (
+					<Button key={room.id} className="cursor-pointer" variant="ghost">
+						<Link to={`/room/${room.id}`}>{room.name}</Link>
+					</Button>
+				))}
+			</div>
 		</div>
 	);
 }
--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,3 +1,23 @@
+import { Navigate, useParams } from "react-router-dom";
+
+type RoomParams = {
+	roomId: string;
+};
+
 export function Room() {
-	return <h1>Detalhes da Sala</h1>;
+	const params = useParams<RoomParams>();
+
+	if (!params.roomId) {
+		return <Navigate replace to="/" />;
+	}
+
+	return (
+		<div className="flex flex-col items-center h-screen w-screen gap-4">
+			<h1 className="text-4xl my-4">Detalhes da Sala</h1>
+
+			<div className="flex justify-center items-center h-full w-full">
+				<p className="animate-spin w-fit">{params.roomId}</p>
+			</div>
+		</div>
+	);
 }
