a0e16e69418104d2f0ed68e6a108cf8d94466256
Bruno Becoski <brunobecoski@gmail.com>
2025-07-10 14:59:46 -0300

    Adiciona o Tailwind CSS e o shadcn/ui

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,26 @@
+import { Wand } from "lucide-react";
+import { Button } from "./components/ui/button";
 export function App() {
-	return <div>Hello Word</div>;
+	return (
+		<div>
+			<Button variant="default">Hello Word</Button>
+			<Button variant="destructive">Hello Word</Button>
+			<Button variant="ghost">Hello Word</Button>
+			<Button variant="link">Hello Word</Button>
+			<Button variant="outline">Hello Word</Button>
+			<Button variant="secondary">Hello Word</Button>
+			<Button size="default">
+				<Wand />
+			</Button>
+			<Button size="icon">
+				<Wand />
+			</Button>
+			<Button size="lg">
+				<Wand />
+			</Button>
+			<Button size="sm">
+				<Wand />
+			</Button>
+		</div>
+	);
 }
