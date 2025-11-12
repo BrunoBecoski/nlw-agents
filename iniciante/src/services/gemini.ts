a91d229badfa45ed1da6d5433c088baf063bc906
Bruno Becoski <brunobecoski@gmail.com>
2025-11-11 21:29:45 -0300

    Atualiza a função generateAnswer

--- iniciante/src/services/gemini.ts
+++ iniciante/src/services/gemini.ts
@@ -17,23 +17,39 @@ export async function generateAnswer({
     apiKey,
   })
 
-  // const tools = [{ googleSearch: {} }]
+  const tools = [{ googleSearch: {} }]
 
   const model = 'gemini-2.5-flash'
 
   const systemInstruction = `
-		## Especialidade
-		- Você é um especialista assistente de meta para o jogo ${game}.
+    Sua tarefa é dupla:
 
-		## Tarefa
-		- Você deve responser as perguntas do usuário com base no seu conhecimento do jogo, estratégias, build e dicas.
+    Os resultados DEVEM ser formatado como um único bloco de código JSON Markdown, **SEM NENHUM OUTRO TEXTO FORA DELE**.
+    O formato do JSON deve ser:
+      json {
+        "answer": "A resposta completa e formatada em Markdown para a pergunta.",
+        "context": "O novo resumo da conversa completa."
+      }
 
-		## Regras
-		- Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
-		- Se a pergunta não está relacionada ao jogo, responda com 'Essa pergunta não está relaciona ao jogo'.
-		- Considere a data atual ${new Date().toLocaleDateString()}
-		- Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
-		- Nunca responda com mecânicas que você não tenha certeza de que existe no patch atual.
+    1) Responda à pergunta ("question") usando o "contextConversation" como contexto.
+      ## Especialidade
+      - Você é um especialista assistente de meta para o jogo ${game}.
+
+      ## Tarefa
+      - Você deve usar o Google Search para pesquisar o patch atual (baseado na data ${new Date().toLocaleDateString()}) para dar a resposta mais coerente e atualizada.
+      - A resposta deve seguir as regras de formatação listadas na descrição do campo "answer" do JSON.
+
+      ## Regras
+      - Se você não sabe a resposta, responda APENAS com 'Não sei' (dentro do campo "answer").
+      - Se a pergunta não está relacionada ao jogo, responda APENAS com 'Essa pergunta não está relacionada ao jogo' (dentro do campo "answer").
+      - Nunca responda com mecânicas que você não tenha certeza de que existe no patch atual.
+      - Não faça saudações ou despedidas.
+      - Máximo de 500 palavras.
+
+    2) Gere o novo resumo da conversa (campo "context" do JSON).
+      ##  Regras
+      - A versão concisa de toda a conversa até agora.
+      - Máximo de 150 palavras.
   `.trim()
 
   const response = await gemini.models.generateContent({
@@ -52,25 +68,8 @@ export async function generateAnswer({
       },
     ],
     config: {
-      // tools,
+      tools,
       systemInstruction,
-      responseMimeType: 'application/json',
-      responseJsonSchema: {
-        type: 'object',
-        properties: {
-          answer: {
-            type: 'string',
-            description:
-              'A responta em markdown, seja direto e responsa no máximo 500 palavras. Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.',
-          },
-          context: {
-            type: 'string',
-            description:
-              'A versão concisa de toda a conversa até agora, com no máximo 150 palavras.',
-          },
-        },
-        required: ['answer', 'context'],
-      },
     },
   })
 
@@ -78,7 +77,11 @@ export async function generateAnswer({
     throw new Error('Falhar ao gerar resposta pelo Gemini')
   }
 
-  const { answer, context } = JSON.parse(response.text)
+  const match = response.text.match(/```json\s*([\s\S]*?)\s*```/)
+  const jsonString = match[1].trim()
+  const parsedObject = JSON.parse(jsonString)
+
+  const { answer, context } = parsedObject
 
   return { answer, context }
 }
