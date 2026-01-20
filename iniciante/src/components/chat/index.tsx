c215e619ef22a7fcd69cbfe6e5e5becab4073845
Bruno Becoski <brunobecoski@gmail.com>
2026-01-20 17:10:30 -0300

    Atualiza o componente Chat

--- iniciante/src/components/chat/index.tsx
+++ iniciante/src/components/chat/index.tsx
@@ -39,17 +39,16 @@ export function Chat({
   }, [animation])
 
   return (
-    <section className="flex h-screen w-1/2 flex-col py-6">
+    <section className="flex h-screen w-screen flex-col p-12 pt-0">
       <Button
-        className={`absolute top-6 left-6 size-12 animate-scale cursor-pointer hover:text-[#9572FC] ${currentAnimation}`}
+        className={`absolute top-6 left-0 size-12 animate-scale cursor-pointer hover:text-[#9572FC] ${currentAnimation}`}
         onClick={handleBackHome}
         title="Voltar"
         variant="link"
       >
         <ChevronLeft className="size-12" />
       </Button>
-
-      <div className="mask-b-from-95% mask-b-to-100% mb-2 h-full space-y-1 overflow-x-hidden overflow-y-scroll pr-2 pb-8 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-[#9572FC]/80 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-[#2A2634] [&::-webkit-scrollbar]:w-2">
+      <div className="mask-b-from-95% mask-b-to-100% mask-t-from-95% mask-t-to-100% my-2 h-full space-y-1 overflow-x-hidden overflow-y-scroll p-4 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-[#9572FC]/80 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-[#2A2634] [&::-webkit-scrollbar]:w-2">
         {questions.map((question) => (
           <Question
             animation={animation}
