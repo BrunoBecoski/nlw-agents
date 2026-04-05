3d7a512a06e18d103721231d480f6241b12c156e
Bruno Becoski <brunobecoski@gmail.com>
2026-04-05 13:47:31 -0300

    Atualiza o seed.ts

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,13 +1,39 @@
 import { reset, seed } from 'drizzle-seed'
-
 import { db, sql } from './connection.ts'
 import { schema } from './schema/index.ts'
 
 await reset(db, schema)
 
 const dummyVector = Array.from({ length: 768 }, () => Math.random())
+const now = new Date()
+const oneYearAgo = new Date()
+oneYearAgo.setFullYear(now.getFullYear() - 1)
 
-await seed(db, { rooms: schema.rooms, questions: schema.questions })
+await seed(db, { rooms: schema.rooms, questions: schema.questions }).refine(
+  (f) => ({
+    rooms: {
+      count: 5,
+      columns: {
+        name: f.companyName(),
+        description: f.loremIpsum(),
+        createdAt: f.date({ minDate: now, maxDate: oneYearAgo }),
+      },
+      with: {
+        questions: [
+          {
+            weight: 1,
+            count: 3,
+            columns: {
+              question: f.loremIpsum(),
+              answer: f.loremIpsum(),
+              createdAt: f.date({ minDate: now, maxDate: oneYearAgo }),
+            },
+          },
+        ] as any,
+      },
+    },
+  })
+)
 
 const allRooms = await db.select().from(schema.rooms)
 
@@ -20,5 +46,4 @@ for (const room of allRooms) {
 }
 
 await sql.end()
-
 console.log('🌱 >>> Database seeded')
