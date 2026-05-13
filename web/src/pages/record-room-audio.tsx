598dd0fb47ee53561dc304e2dbd8a5f43fafd8ef
Bruno Becoski <brunobecoski@gmail.com>
2026-05-13 20:07:02 -0300

    Atualiza o componente Stopwatch

--- web/src/pages/record-room-audio.tsx
+++ web/src/pages/record-room-audio.tsx
@@ -3,6 +3,8 @@ import { Navigate, useParams } from 'react-router-dom'
 import { Stopwatch } from '@/components/stopwatch'
 import { Button } from '@/components/ui/button'
 
+type Status = '' | 'recording' | 'pause' | 'stop'
+
 type RecordRoomParams = {
   roomId: string
 }
@@ -18,6 +20,7 @@ export function RecordRoomAudio() {
   const recorderRef = useRef<MediaRecorder | null>(null)
   const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null)
 
+  const [status, setStatus] = useState<Status>('')
   const [isRecording, setIsRecording] = useState(false)
   const [recording, setRecording] = useState('')
   const [time, setTime] = useState(0)
@@ -47,26 +50,38 @@ export function RecordRoomAudio() {
     })
 
     recorderRef.current.ondataavailable = (event) => {
+      console.log('Gravação obtida')
       setRecording(URL.createObjectURL(event.data))
     }
 
     recorderRef.current.onstart = () => {
-      console.log('Gravação iniciada!')
+      console.log('Gravação iniciada')
+    }
+
+    recorderRef.current.onresume = () => {
+      console.log('Gravação continuada')
+    }
+
+    recorderRef.current.onpause = () => {
+      console.log('Gravação pausada')
     }
 
     recorderRef.current.onstop = () => {
-      console.log('Gravação encerrada/pausada')
+      console.log('Gravação parada')
     }
 
     recorderRef.current.start()
   }
 
   async function startRecording() {
+    setStatus('recording')
+
     if (!isRecordingSupported) {
       alert('O seu navegador não suporta gravação')
       return
     }
 
+    setTime(0)
     setIsRecording(true)
 
     const audio = await navigator.mediaDevices.getUserMedia({
@@ -81,10 +96,32 @@ export function RecordRoomAudio() {
   }
 
   function stopRecording() {
+    setStatus('stop')
+
     setIsRecording(false)
 
     if (recorderRef.current && recorderRef.current.state !== 'inactive') {
       recorderRef.current.stop()
+    }
+  }
+
+  function pauseRecording() {
+    setStatus('pause')
+
+    setIsRecording(false)
+
+    if (recorderRef.current && recorderRef.current.state === 'recording') {
+      recorderRef.current.pause()
+    }
+  }
+
+  function resumeRecording() {
+    setStatus('recording')
+
+    setIsRecording(true)
+
+    if (recorderRef.current && recorderRef.current.state === 'paused') {
+      recorderRef.current.resume()
     }
   }
 
@@ -93,12 +130,9 @@ export function RecordRoomAudio() {
       intervalRef.current = setInterval(() => {
         setTime((prev) => prev + 1)
       }, 10)
-    } else {
-      if (intervalRef.current) {
-        clearInterval(intervalRef.current)
-        intervalRef.current = null
-      }
-      setTime(0)
+    } else if (intervalRef.current) {
+      clearInterval(intervalRef.current)
+      intervalRef.current = null
     }
 
     return () => {
@@ -114,22 +148,79 @@ export function RecordRoomAudio() {
 
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-3">
-      {isRecording ? (
-        <>
-          <Button className="cursor-pointer" onClick={stopRecording} size="lg">
-            Pausar gravação
-          </Button>
-          <p className="animate-pulse text-red-500 italic">Gravando...</p>
-        </>
-      ) : (
+      {status === '' && (
         <Button className="cursor-pointer" onClick={startRecording} size="lg">
           Gravar áudio
         </Button>
       )}
 
-      <Stopwatch time={time} />
+      {status === 'recording' && (
+        <div>
+          <Stopwatch time={time} />
+          <p className="animate-pulse text-center text-red-500 italic">
+            Gravando...
+          </p>
 
-      {recording && <audio controls src={recording} />}
+          <div className="flex gap-8">
+            <Button
+              className="cursor-pointer"
+              onClick={pauseRecording}
+              size="lg"
+            >
+              Pausar gravação
+            </Button>
+
+            <Button
+              className="cursor-pointer"
+              onClick={stopRecording}
+              size="lg"
+            >
+              Parar gravação
+            </Button>
+          </div>
+        </div>
+      )}
+
+      {status === 'pause' && (
+        <div>
+          <Stopwatch time={time} />
+          <p className="animate-pulse text-center text-red-500 italic">
+            Gravação pausada
+          </p>
+
+          <div className="flex gap-8">
+            <Button
+              className="cursor-pointer"
+              onClick={resumeRecording}
+              size="lg"
+            >
+              Voltar gravação
+            </Button>
+
+            <Button
+              className="cursor-pointer"
+              onClick={stopRecording}
+              size="lg"
+            >
+              Parar gravação
+            </Button>
+          </div>
+        </div>
+      )}
+
+      {status === 'stop' && (
+        <div className="flex flex-col gap-8">
+          <Button className="cursor-pointer" onClick={startRecording} size="lg">
+            Começar outro gravação
+          </Button>
+
+          <audio controls src={recording} />
+
+          <Button className="cursor-pointer" size="lg">
+            Mandar a api
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
